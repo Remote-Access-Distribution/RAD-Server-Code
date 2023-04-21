77173fc22b26f38360867f1681e80acbd2850880
Remote-Access-Distribution <131410211+Remote-Access-Distribution@users.noreply.github.com>
2023-04-21 13:07:55 +0100

    Fixed Setup Script

--- 1 - Setup.bat
+++ 1 - Setup.bat
@@ -3,5 +3,6 @@ python-3.11.0-amd64.exe
 mosquitto-2.0.15-install-windows-x64.exe
 
 cd..
+python -m venv myenv
 myenv\Scripts\python.exe -m pip install --upgrade pip
 myenv\Scripts\pip.exe install -r requirements.txt
